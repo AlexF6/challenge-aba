deb8e2f41495f8edeeae1157e1b076d6269b73fd
AlexF6 <perol2894@gmail.com>
2025-07-04 14:19:27 -0500

    fix: note containers styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/layout/header/Header.jsx"
 import Note from './components/layout/note-list/Note.jsx';
 import CreateButton from './components/layout/note-list/CreateButton.jsx';
 import NoteButtons from './components/layout/note-actions/NoteButtons.jsx';
+import NoteList from './components/layout/note-list/NoteList.jsx';
 
 function App() {
   
@@ -16,25 +17,16 @@ function App() {
         <div className='main-section'>
 
           <div className='note-list'>
-            <CreateButton/>
-            <Note content="Primera nota de prueba xd" tag1="Dev" tag2="Travel" date="24 Junio 2025"/>
-            <Note content="Segunda nota de prueba" tag1="Dev" tag2="Travel" date="24 Junio 2025"/>
-            <Note content="Tercera nota de prueba" tag1="Dev" tag2="Travel" date="24 Junio 2025"/>
-            <Note content="Cuarta nota de prueba" tag1="Dev" tag2="Travel" date="24 Junio 2025"/>
-            <Note content="Quinta nota de prueba" tag1="Dev" tag2="Travel" date="24 Junio 2025"/>
+            <NoteList/>
           </div>
-
           <div className='note-details'>
-
+            
           </div>
-
           <div className='note-actions'>
-            <NoteButtons/>
-                          
+              <NoteButtons/>
           </div>
 
         </div>
-
         </div>
       </div>
 
